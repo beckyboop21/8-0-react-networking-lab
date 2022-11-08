211348e816fd6844719e64ff32284a628fd09e2a
Rebecca Garcia <shelteredheart21@gmail.com>
2022-11-07 23:54:19 -0500

    test working

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,28 @@
-import React from "react";
+// import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+
 export const EmployeeList = () => {
+const [PetEmpl,setPetEmpl]=useState([])
+  useEffect(()=>{
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/employees")
+    .then((res)=>res.json())
+    .then((res)=>setPetEmpl(res))
+  },[])
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+       
+       { PetEmpl.map((employee)=>{
+        return <Employee employee={employee} />
+       })
+
+}
       </section>
     </main>
   );
 };
-
 export default EmployeeList;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,28 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+
+export const PetList = ({ id }) => {
+  const [emplPets, setEmplPets] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://vet-resource-api-9-2.herokuapp.com/api/pets?employeeId=${id}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setEmplPets(res);
+        console.log(res);
+      });
+  }, [id]);
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      
+      {emplPets.length ? (
+        emplPets.map((pet) => <p>{pet.name}</p>)
+      ) : (
+        <p>No pets listed for this employee.</p>
+      )}
     </aside>
   );
 };
-
 export default PetList;
